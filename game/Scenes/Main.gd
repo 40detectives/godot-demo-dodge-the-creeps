9849d690741a5da7dc506e057055d298cb15c318
40detectives <frank1991@gmail.com>
2019-10-21 17:30:57 +0200

    Connect HUD functions to Main Scene

--- game/Scenes/Main.gd
+++ game/Scenes/Main.gd
@@ -15,14 +15,15 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_StartTimer_timeout():
@@ -32,6 +33,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_MobTimer_timeout():
@@ -50,7 +52,7 @@ func _on_MobTimer_timeout():
 	# Set the velocity (speed & direction)
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
-	
+
 
 
 
